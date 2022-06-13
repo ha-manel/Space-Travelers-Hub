4576be99b102795cf37d86e02ea9f43a96c1cb99
Ronald <rjskinnerc@unal.edu.co>
2022-06-13 17:58:12 -0500

    Add async call to the dragons endpoint

--- src/redux/dragons/dragons.js
+++ src/redux/dragons/dragons.js
@@ -1,12 +1,23 @@
 import axios from 'axios';
 
-const fetchDragons = async () => {
+const DRAGONS_FETCHED = 'DRAGONS_FETCHED';
+
+async function fetchDragons(dispatch) {
   const { data } = await axios.get('https://api.spacexdata.com/v3/dragons');
-  console.log(data);
-};
+  const dragons = data.map((dragon) => ({
+    id: dragon.id,
+    name: dragon.name,
+    type: dragon.type,
+    flickr_images: dragon.flickr_images[0],
+  }));
+
+  dispatch({ type: DRAGONS_FETCHED, payload: dragons });
+}
 
 export default function dragons(state = [], action) {
   switch (action.type) {
+    case DRAGONS_FETCHED:
+      return action.payload;
     default:
       return state;
   }
