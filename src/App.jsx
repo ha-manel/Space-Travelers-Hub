4752fe18a67614158c6b36c9c2de25df7d0992e0
Manel <manelhammouche3@gmail.com>
2022-06-13 21:10:40 +0100

    Extract navbar as a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
-import { Navbar, Container } from 'react-bootstrap';
 import './App.css';
-import logo from './assets/logo.png';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="light" variant="light">
-        <Container>
-          <Navbar.Brand href="#rockets">
-            <img
-              alt="Cool logo."
-              src={logo}
-              width="36"
-              height="36"
-              className="d-inline-block align-center"
-            />
-            {' '}
-            <span className="title">Space Travelers&apos; Hub</span>
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
+      <Navbar />
     </div>
   );
 }
