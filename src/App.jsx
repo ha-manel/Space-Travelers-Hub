7f6e79eacde7b5a210e9a9d3d77bb320a14f0fb5
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-13 17:30:51 -0500

    Add component navigation for rockets

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import Nav from './components/nav/Nav';
 import MyProfile from './components/my-profile/MyProfile';
 import Dragons from './components/dragons/Dragons';
+import Rockets from './components/rockets/Rockets';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Routes>
+        <Route path="/rockets" element={<Rockets />} />
         <Route path="/dragons" element={<Dragons />} />
         <Route path="/profile" element={<MyProfile />} />
       </Routes>
