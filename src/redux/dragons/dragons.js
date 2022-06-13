3bd0951a4cc6b0701028b3aa5d4216465de3960a
Ronald <rjskinnerc@unal.edu.co>
2022-06-13 18:15:32 -0500

    Check if there are dragons in the store before new call

--- src/redux/dragons/dragons.js
+++ src/redux/dragons/dragons.js
@@ -2,16 +2,20 @@ import axios from 'axios';
 
 const DRAGONS_FETCHED = 'DRAGONS_FETCHED';
 
-async function fetchDragons(dispatch) {
-  const { data } = await axios.get('https://api.spacexdata.com/v3/dragons');
-  const dragons = data.map((dragon) => ({
-    id: dragon.id,
-    name: dragon.name,
-    type: dragon.type,
-    flickr_images: dragon.flickr_images[0],
-  }));
+async function fetchDragons(dispatch, getState) {
+  const { dragons: currentDragons } = getState();
 
-  dispatch({ type: DRAGONS_FETCHED, payload: dragons });
+  if (currentDragons.length === 0) {
+    const { data } = await axios.get('https://api.spacexdata.com/v3/dragons');
+    const fetchedDragons = data.map((dragon) => ({
+      id: dragon.id,
+      name: dragon.name,
+      type: dragon.type,
+      flickr_images: dragon.flickr_images[0],
+    }));
+
+    dispatch({ type: DRAGONS_FETCHED, payload: fetchedDragons });
+  }
 }
 
 export default function dragons(state = [], action) {
