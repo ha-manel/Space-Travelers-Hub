75d175b4f10140b4d435d5a08f96e2069dd784dc
Ronald <rjskinnerc@unal.edu.co>
2022-06-13 16:54:31 -0500

    Add fetchDragons basic function and component call

--- src/redux/dragons/dragons.js
+++ src/redux/dragons/dragons.js
@@ -1,6 +1,15 @@
+import axios from 'axios';
+
+const fetchDragons = async () => {
+  const { data } = await axios.get('https://api.spacexdata.com/v3/dragons');
+  console.log(data);
+};
+
 export default function dragons(state = [], action) {
   switch (action.type) {
     default:
       return state;
   }
 }
+
+export { fetchDragons };
