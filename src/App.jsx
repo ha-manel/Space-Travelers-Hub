f974aca3e26aeea84bfb08a80a9660f46ec2c1a3
Manel <manelhammouche3@gmail.com>
2022-06-14 13:30:46 +0100

    Dispatch loading missions action

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Nav from './components/nav/Nav';
 import MyProfile from './components/my-profile/MyProfile';
 import Dragons from './components/dragons/Dragons';
 import Rockets from './components/rockets/Rockets';
 import Missions from './components/missions/Missions';
+import { fetchMissions } from './redux/missions/missions';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchMissions);
+  }, []);
+
   return (
     <div className="App">
       <Nav />
