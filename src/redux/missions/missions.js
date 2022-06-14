2ff166559e5a0ac866b5100419af3af6b35d53a9
Manel <manelhammouche3@gmail.com>
2022-06-14 16:39:32 +0100

    Use axios service

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,27 +1,33 @@
-import axios from 'axios';
+import SpacesxService from '../../services/SpacexService';
 
-// Actions
+// Action
 const LOAD_MISSIONS = 'spacehub/missions/LOAD_MISSIONS';
 
 // Reducer
 export default function missions(state = [], action = {}) {
   switch (action.type) {
+    case LOAD_MISSIONS:
+      return action.payload;
     default:
       return state;
   }
 }
 
 // Side Effects
-const fetchMissions = async (dispatch) => {
-  const { data } = await axios.get('https://api.spacexdata.com/v3/missions');
-  const missions = data.map((mission) => (
-    {
-      mission_id: mission.mission_id,
-      mission_name: mission.mission_name,
-      description: mission.description,
-    }
-  ));
-  dispatch({ type: LOAD_MISSIONS, payload: missions });
+const fetchMissions = async (dispatch, getState) => {
+  const currentMissions = getState().missions;
+
+  if (currentMissions.length === 0) {
+    const { data } = await SpacesxService.getMissions();
+    const missions = data.map((mission) => (
+      {
+        mission_id: mission.mission_id,
+        mission_name: mission.mission_name,
+        description: mission.description,
+      }
+    ));
+    dispatch({ type: LOAD_MISSIONS, payload: missions });
+  }
 };
 
 export { fetchMissions };
