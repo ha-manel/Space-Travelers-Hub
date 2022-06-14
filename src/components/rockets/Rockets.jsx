864d59c5dc739c12758cdf65d1c229e1fb7f9214
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-14 10:39:40 -0500

    Add display list of rockets

--- src/components/rockets/Rockets.jsx
+++ src/components/rockets/Rockets.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getRockets } from '../../redux/rockets/rockets';
+
+import './Rockets.css';
 
 const Rockets = () => {
   const dispatch = useDispatch();
@@ -9,8 +11,19 @@ const Rockets = () => {
     dispatch(getRockets);
   }, []);
 
+  const rockets = useSelector((state) => state.rockets);
+
   return (
-    <div>Rockets</div>
+    <div className="rockets">
+      <ul className="rocket-list">
+        {rockets && rockets.map((rocket) => (
+          <li className="rocket" key={rocket.id}>
+            <img src={rocket.flickr_images} alt={rocket.rocket_name} className="rocket-img" />
+            <span className="rocket-description">{rocket.description}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
