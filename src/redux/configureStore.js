3cbe2d694ebb82c24dcc6d6107332cc1a5566446
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-13 21:29:58 -0500

    Add rockets to store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,9 +2,10 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import dragons from './dragons/dragons';
+import rockets from './rockets/rockets';
 
 const rootReducer = combineReducers({
-  // rockets,
+  rockets,
   dragons,
   // missions,
 });
