89f16b9e586364ac4652d89cef0761496dcb48b9
Manel <manelhammouche3@gmail.com>
2022-06-14 13:29:28 +0100

    Update table structure

--- src/components/missions/Missions.jsx
+++ src/components/missions/Missions.jsx
@@ -1,35 +1,38 @@
-import React from 'react';
 import './missions.css';
 
 const Missions = () => (
   <div id="missions-section">
     <table>
-      <tr>
-        <th>Mission</th>
-        <th>Description</th>
-        <th>Status</th>
-        <th> </th>
-      </tr>
-      <tr>
-        <td>Telstar</td>
-        <td>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo esse nihil consequatur
-          necessitatibus. Error placeat dolorum qui laboriosam soluta vitae fugiat, aperiam
-          recusandae eaque sit nulla numquam similique minus cum.
-        </td>
-        <td>active member</td>
-        <td>join mission</td>
-      </tr>
-      <tr>
-        <td>Telstar</td>
-        <td>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo esse nihil consequatur
-          necessitatibus. Error placeat dolorum qui laboriosam soluta vitae fugiat, aperiam
-          recusandae eaque sit nulla numquam similique minus cum.
-        </td>
-        <td>active member</td>
-        <td>join mission</td>
-      </tr>
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th> </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Telstar</td>
+          <td>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo esse nihil consequatur
+            necessitatibus. Error placeat dolorum qui laboriosam soluta vitae fugiat, aperiam
+            recusandae eaque sit nulla numquam similique minus cum.
+          </td>
+          <td>active member</td>
+          <td>join mission</td>
+        </tr>
+        <tr>
+          <td>Telstar</td>
+          <td>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo esse nihil consequatur
+            necessitatibus. Error placeat dolorum qui laboriosam soluta vitae fugiat, aperiam
+            recusandae eaque sit nulla numquam similique minus cum.
+          </td>
+          <td>active member</td>
+          <td>join mission</td>
+        </tr>
+      </tbody>
     </table>
   </div>
 );
