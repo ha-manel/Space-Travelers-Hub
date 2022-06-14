e41c23115c348b17a8de3aaf27b76547759c8369
Manel <manelhammouche3@gmail.com>
2022-06-14 11:45:48 +0100

    Add missions basic view structure

---
+++ src/components/missions/Missions.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './missions.css';
+
+const Missions = () => (
+  <div id="missions-section">
+    <table>
+      <tr>
+        <th>Mission</th>
+        <th>Description</th>
+        <th>Status</th>
+        <th> </th>
+      </tr>
+      <tr>
+        <td>Telstar</td>
+        <td>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo esse nihil consequatur
+          necessitatibus. Error placeat dolorum qui laboriosam soluta vitae fugiat, aperiam
+          recusandae eaque sit nulla numquam similique minus cum.
+        </td>
+        <td>active member</td>
+        <td>join mission</td>
+      </tr>
+      <tr>
+        <td>Telstar</td>
+        <td>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo esse nihil consequatur
+          necessitatibus. Error placeat dolorum qui laboriosam soluta vitae fugiat, aperiam
+          recusandae eaque sit nulla numquam similique minus cum.
+        </td>
+        <td>active member</td>
+        <td>join mission</td>
+      </tr>
+    </table>
+  </div>
+);
+
+export default Missions;
