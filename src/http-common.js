21dea2fb7cc9b40c99dcf915c667d08e9e9a1e2b
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-13 21:30:07 -0500

    Fix eslint errors

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
+
 export default axios.create({
-  baseURL: "https://api.spacexdata.com/v3",
+  baseURL: 'https://api.spacexdata.com/v3',
   headers: {
-    "Content-type": "application/json"
-  }
+    'Content-type': 'application/json',
+  },
 });
