f86111bad299a4b6cc596337350a080381be0ef9
Manel <manelhammouche3@gmail.com>
2022-06-14 16:38:50 +0100

    Dispatch loading missions action

--- src/components/missions/Missions.jsx
+++ src/components/missions/Missions.jsx
@@ -1,40 +1,50 @@
 import './missions.css';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchMissions } from '../../redux/missions/missions';
 
-const Missions = () => (
-  <div id="missions-section">
-    <table>
-      <thead>
-        <tr>
-          <th>Mission</th>
-          <th>Description</th>
-          <th>Status</th>
-          <th> </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Telstar</td>
-          <td>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo esse nihil consequatur
-            necessitatibus. Error placeat dolorum qui laboriosam soluta vitae fugiat, aperiam
-            recusandae eaque sit nulla numquam similique minus cum.
-          </td>
-          <td>active member</td>
-          <td>join mission</td>
-        </tr>
-        <tr>
-          <td>Telstar</td>
-          <td>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo esse nihil consequatur
-            necessitatibus. Error placeat dolorum qui laboriosam soluta vitae fugiat, aperiam
-            recusandae eaque sit nulla numquam similique minus cum.
-          </td>
-          <td>active member</td>
-          <td>join mission</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-);
+const Missions = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchMissions);
+  }, []);
+
+  return (
+    <div id="missions-section">
+      <table>
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th> </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Telstar</td>
+            <td>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo esse nihil consequatur
+              necessitatibus. Error placeat dolorum qui laboriosam soluta vitae fugiat, aperiam
+              recusandae eaque sit nulla numquam similique minus cum.
+            </td>
+            <td>active member</td>
+            <td>join mission</td>
+          </tr>
+          <tr>
+            <td>Telstar</td>
+            <td>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo esse nihil consequatur
+              necessitatibus. Error placeat dolorum qui laboriosam soluta vitae fugiat, aperiam
+              recusandae eaque sit nulla numquam similique minus cum.
+            </td>
+            <td>active member</td>
+            <td>join mission</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Missions;
