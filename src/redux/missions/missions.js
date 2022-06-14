9db1946d96fd47b0e2f0773704ed14140a4d38ad
Manel <manelhammouche3@gmail.com>
2022-06-14 13:28:36 +0100

    Fetch missions from the API

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,8 +1,27 @@
-const missions = (state = [], action) => {
+import axios from 'axios';
+
+// Actions
+const LOAD_MISSIONS = 'spacehub/missions/LOAD_MISSIONS';
+
+// Reducer
+export default function missions(state = [], action = {}) {
   switch (action.type) {
     default:
       return state;
   }
+}
+
+// Side Effects
+const fetchMissions = async (dispatch) => {
+  const { data } = await axios.get('https://api.spacexdata.com/v3/missions');
+  const missions = data.map((mission) => (
+    {
+      mission_id: mission.mission_id,
+      mission_name: mission.mission_name,
+      description: mission.description,
+    }
+  ));
+  dispatch({ type: LOAD_MISSIONS, payload: missions });
 };
 
-export default missions;
+export { fetchMissions };
