26ebc3a6163c4260d337a481658af4c6444e70c5
Ronald <rjskinnerc@unal.edu.co>
2022-06-15 17:18:58 -0500

    Fix linter errors

--- src/components/dragons/dragons.test.js
+++ src/components/dragons/dragons.test.js
@@ -1,62 +1,64 @@
-import { render, screen, waitFor, fireEvent, cleanup } from '@testing-library/react';
+import {
+  render, screen, waitFor, fireEvent, act,
+} from '@testing-library/react';
 import { Provider } from 'react-redux';
 import Dragons from './Dragons';
 import store from '../../redux/configureStore';
-import axios from '../../http-common'
+import axios from '../../http-common';
+
 jest.mock('../../http-common');
 
-describe("The Dragons page component", () => {
+describe('The Dragons page component', () => {
   beforeEach(async () => {
     const resp = {
       data: [
         {
-          "id": "dragon1",
-          "name": "Dragon 1",
-          "type": "capsule",
-          "flickr_images": [
-            "https://i.imgur.com/9fWdwNv.jpg",
+          id: 'dragon1',
+          name: 'Dragon 1',
+          type: 'capsule',
+          flickr_images: [
+            'https://i.imgur.com/9fWdwNv.jpg',
           ],
         },
         {
-          "id": "dragon2",
-          "name": "Dragon 2",
-          "type": "capsule",
-          "flickr_images": [
-            "https://farm8.staticflickr.com/7647/16581815487_6d56cb32e1_b.jpg",
+          id: 'dragon2',
+          name: 'Dragon 2',
+          type: 'capsule',
+          flickr_images: [
+            'https://farm8.staticflickr.com/7647/16581815487_6d56cb32e1_b.jpg',
           ],
-        }
-      ]
+        },
+      ],
     };
-    await axios.get.mockResolvedValue(resp)
+    await axios.get.mockResolvedValue(resp);
   });
 
   afterEach(() => {
-    store.dispatch({
-      type: "spacehub/dragons/DRAGONS_FETCHED",
+    act(() => store.dispatch({
+      type: 'spacehub/dragons/DRAGONS_FETCHED',
       payload: [],
-    });
+    }));
   });
 
-  it("should render the page", async () => {
-    render(<Provider store={store}><Dragons /></Provider>)
+  it('should render the page', async () => {
+    render(<Provider store={store}><Dragons /></Provider>);
     await waitFor(() => {
       expect(screen.getAllByText('Reserve Dragon').length).toBeGreaterThan(0);
     });
-
   });
 
-  it("becomes a reserved dragon after a user clicks its reserve button", async () => {
+  it('becomes a reserved dragon after a user clicks its reserve button', async () => {
     render(<Provider store={store}><Dragons /></Provider>);
     const buttons = await screen.findAllByText('Reserve Dragon');
     fireEvent.click(buttons[0]);
 
-    const reservedBadge = await screen.findAllByText('Reserved')
-    const cancelButtons = await screen.findAllByText('Cancel Reservation')
+    const reservedBadge = await screen.findAllByText('Reserved');
+    const cancelButtons = await screen.findAllByText('Cancel Reservation');
     expect(reservedBadge.length).toBe(1);
     expect(cancelButtons.length).toBe(1);
   });
 
-  it("should cancel a reservation when the user clicks the cancel reservation button", async () => {
+  it('should cancel a reservation when the user clicks the cancel reservation button', async () => {
     render(<Provider store={store}><Dragons /></Provider>);
     const reserveButtons = await screen.findAllByText('Reserve Dragon');
     fireEvent.click(reserveButtons[0]);
@@ -65,7 +67,6 @@ describe("The Dragons page component", () => {
     fireEvent.click(cancelButtons[0]);
 
     const buttons = await screen.findAllByText('Reserve Dragon');
-
-    expect(buttons.length).toBe(2)
-  })
-})
+    expect(buttons.length).toBe(2);
+  });
+});
