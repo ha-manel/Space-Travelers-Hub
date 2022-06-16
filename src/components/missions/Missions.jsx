0c46e7b53aa2a5fea1eefba32396a0b2d1002448
Manel <manelhammouche3@gmail.com>
2022-06-16 16:50:55 +0100

    Add modal window for mobile view

--- src/components/missions/Missions.jsx
+++ src/components/missions/Missions.jsx
@@ -1,9 +1,16 @@
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 import './missions.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchMissions, changeStatus } from '../../redux/missions/missions';
+import Description from './Description';
 
 const Missions = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const [modalMission, setModalMission] = useState({
+    name: '',
+    description: '',
+  });
   const missions = useSelector((state) => state.missions);
   const dispatch = useDispatch();
 
@@ -15,13 +22,21 @@ const Missions = () => {
     dispatch(changeStatus(id));
   };
 
+  const showModal = (name, description) => {
+    setOpenModal(true);
+    setModalMission({
+      name,
+      description,
+    });
+  };
+
   return (
-    <div id="missions-section">
+    <div id="missions-section" style={{ overflow: openModal ? 'hidden' : 'scroll' }}>
       <table>
         <thead>
           <tr>
             <th>Mission</th>
-            <th>Description</th>
+            <th className="desc-header">Description</th>
             <th>Status</th>
             <th> </th>
           </tr>
@@ -29,14 +44,26 @@ const Missions = () => {
         <tbody>
           {missions.map((mission) => (
             <tr key={mission.mission_id}>
-              <td className="mission-name">{mission.mission_name}</td>
-              <td>{mission.description}</td>
+              <td className="mission-name">
+                {mission.mission_name}
+                <button
+                  type="button"
+                  className="see-more"
+                  onClick={() => showModal(mission.mission_name, mission.description)}
+                >
+                  See more
+                </button>
+              </td>
+
+              <td className="mission-desc">{mission.description}</td>
+
               <td className="status">
                 {!mission.reserved && <span>NOT A MEMBER</span>}
                 {mission.reserved && (
                   <span style={{ backgroundColor: '#18a2b8' }}>Active Member</span>
                 )}
               </td>
+
               <td className="join-mission">
                 <button
                   type="button"
@@ -52,6 +79,14 @@ const Missions = () => {
               </td>
             </tr>
           ))}
+          {openModal && (
+            <Description
+              closeModal={setOpenModal}
+              name={modalMission.name}
+              description={modalMission.description}
+            />
+          )}
+          {openModal ? disableBodyScroll(document) : enableBodyScroll(document)}
         </tbody>
       </table>
     </div>
